6d5668c4c0d505295a7389818691010e1c730e00
Your Name <you@example.com>
2016-08-10 09:42:16 +0600

    WEBAPIApplication
    WEB API Application

---
+++ CRUDWebAPI/CRUDWebAPI_Client/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUDWebAPI_Client.Models;
+using CRUDWebAPI_Client.ViewModels;
+
+namespace CRUDWebAPI_Client.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+        public ActionResult Index()
+        {
+            ProductCleint pc = new ProductCleint();
+            ViewBag.listProducts = pc.findAll();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+           
+            return View("Create");
+        }
+
+        [HttpPost]
+        public ActionResult Create(ProductViewModel pvm)
+        {
+            pvm.Product.CreationDate = DateTime.Now;
+            ProductCleint pc = new ProductCleint();
+            pc.Create(pvm.Product);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            ProductCleint pc = new ProductCleint();
+            pc.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            ProductCleint pc = new ProductCleint();
+            ProductViewModel pvm = new ProductViewModel();
+            pvm.Product = pc.find(id);
+            return View("Edit",pvm);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ProductViewModel pvm)
+        {
+            ProductCleint pc = new ProductCleint();
+            pc.Edit(pvm.Product);
+            return RedirectToAction("Index");
+        }
+	}
+}
---
+++ CRUDWebAPI/CRUDWebAPI_Client/ViewModels/ProductViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CRUDWebAPI_Client.Models;
+
+namespace CRUDWebAPI_Client.ViewModels
+{
+    public class ProductViewModel
+    {
+        public Product Product
+        {
+            get;
+            set;
+        }
+    }
+}
